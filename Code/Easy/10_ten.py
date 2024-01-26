5c02992947742ce7c367a12fb444443f20feea26
Krishna Saw <krishnakrsaw8340@gmail.com>
2024-01-26 13:21:20 +0530

    ten

---
+++ Code/Easy/10_ten.py
@@ -0,0 +1,10 @@
+# Raising an exception :
+l =  [1,2,3,4]
+sum  =0
+for i in l:
+    if i==1:
+        raise Exception ("Excption : 1 is found")
+    else:
+        sum +=1
+# Exception: Excption : 1 is found
+
